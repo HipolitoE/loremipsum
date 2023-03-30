d2e8205db90ad424fd57c798b3a0844d400d9a05
HipolitoE <espositohipolito@gmail.com>
2023-03-30 13:20:08 -0300

    5 archivos de txt

--- lipsum.sh
+++ lipsum.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-lorem=$(curl -s https://www.lipsum.com/feed/xml?amount=2 | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g')
+for i in {1..5}
+do
+    lorem=$(curl -s https://www.lipsum.com/feed/xml?amount=2 | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g')
 
-echo "$lorem" > lorem.txt 
+    echo "$lorem" >> ./txt/lorem-$i.txt 
+done
+
